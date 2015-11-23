b4bd961f7e67e7cdf13f79292ec77e81570dac3b
Chris Williams <chrispwill@gmail.com>
2015-11-24 00:17:50 +1100

    Useable xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,39 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
 
-main = xmonad defaultConfig
-    { terminal    = "urxvt"
-    , modMask     = mod4Mask
-    , borderWidth = 2
-    }
+import System.IO
+
+myManagementHooks :: [ManageHook]
+myManagementHooks = [
+  resource =? "stalonetray" --> doIgnore
+  ]
+
+main :: IO ()
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { terminal    = "urxvt"
+        , modMask     = mod4Mask
+        , borderWidth = 2
+
+        , manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll myManagementHooks
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "blue" "" . shorten 50
+                        , ppCurrent = xmobarColor "blue" "" . wrap "[" "]"
+                        }
+        } `additionalKeys`
+        [ 
+         -- Custom Hotkeys
+         -- > Launching/Termination
+          ((mod4Mask,               xK_r     ), spawn "dmenu_run")
+        , ((mod4Mask,               xK_d     ), kill)
+         -- > Application shortcuts
+        , ((mod4Mask,               xK_Return), spawn $ "urxvt")    -- Terminal
+         -- > Web
+        , ((mod4Mask,               xK_f     ), spawn "firefox")
+        ]
