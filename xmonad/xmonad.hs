ee3e666283cc24d57c658bd954f76d78785873ca
Christopher Williams <chrispwill@gmail.com>
2016-08-13 19:00:46 +1000

    xmonad stuff

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,50 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.SimpleFloat
+
+import System.IO
+
+myManagementHooks :: [ManageHook]
+myManagementHooks = [
+  resource =? "stalonetray" --> doIgnore,
+  isFullscreen --> doFullFloat
+  ]
+
+main :: IO ()
+main = do
+    xmproc <- spawnPipe "xmobar"
+    spawn "xscreensaver -no-splash"
+    spawn "stalonetray"
+    spawn "feh --bg-scale /home/cpw/.config/awesome/themes/cpw/wallpaper2.jpg &"
+    spawn "nm-applet"
+    spawn "redshift-gtk"
+    xmonad $ defaultConfig
+        { terminal           = "urxvt"
+        , modMask            = mod4Mask
+        , borderWidth        = 1
+        , normalBorderColor  = "#eeeaea"
+        , focusedBorderColor = "#654dff"
+
+        , manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll myManagementHooks
+        , layoutHook = avoidStruts  $  (simpleFloat ||| layoutHook defaultConfig)
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "blue" "" . shorten 50
+                        , ppCurrent = xmobarColor "blue" "" . wrap "[" "]"
+                        }
+        } `additionalKeys`
+        [ 
+         -- Custom Hotkeys
+         -- > Launching/Termination
+          ((mod4Mask,               xK_r     ), spawn "dmenu_run -nb '#fffafa' -nf '#999999' -l 5")
+        , ((mod4Mask,               xK_d     ), kill)
+        , ((mod4Mask,               xK_z     ), spawn "xscreensaver-command -lock; xset dpms force off")
+         -- > Application shortcuts
+        , ((mod4Mask,               xK_Return), spawn "urxvt")    -- Terminal
+         -- > Web
+        , ((mod4Mask,               xK_f     ), spawn "firefox")
+        ]
